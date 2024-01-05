6e86bad72ddf2956cd56cfdf7190e4abec313694
George Craciun-Vasilache <georgecraciun1998@gmail.com>
2024-01-05 15:54:42 +0200

    fix conflicts

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,122 @@
+import { PrismaClient } from '@prisma/client';
+import {
+  jobs,
+  skills,
+  users,
+  benefits,
+  bestCompany,
+  worstCompany,
+  goodFeedback,
+  badFeedback,
+} from './seed_data.json';
+
+const client = new PrismaClient();
+
+const seed = async () => {
+  for await (const user of users) {
+    await client.users.create({
+      data: {
+        ...user,
+        skills: {
+          createMany: { data: [...skills] },
+        },
+        feedbacks: {
+          create: [
+            {
+              ...(Math.round(Math.random() * 10) % 2 === 0
+                ? goodFeedback
+                : badFeedback),
+              job: {
+                create: {
+                  title: 'Software Engineer',
+                  description: 'Database Administrator',
+                  applicantsCount: 0,
+                },
+              },
+            },
+          ],
+        },
+      },
+    });
+  }
+
+  for (const job of jobs) {
+    await client.jobs.create({
+      data: {
+        ...job,
+        skills: {
+          createMany: { data: [...skills] },
+        },
+        benefits: {
+          createMany: {
+            data: [...benefits],
+          },
+        },
+        company: {
+          create: {
+            ...(Math.round(Math.random() * 10) % 2 === 0
+              ? bestCompany
+              : worstCompany),
+          },
+        },
+      },
+    });
+  }
+
+  //   const users = await client.users.create({
+  //     data: {
+  //       role: 'Applicant',
+  //       username: 'geo98',
+  //       firstName: 'George',
+  //       lastName: 'Craciun',
+  //       hobbies: 'PC Games, cooking',
+  //       lastEducation: 'Masters',
+  //       skills: {
+  //         createMany: {
+  //           data: [
+  //             {
+  //               name: 'JavaScript',
+  //               seniority: 'Senior',
+  //             },
+  //             {
+  //               name: 'CSS',
+  //               seniority: 'Mid',
+  //             },
+  //             {
+  //               name: 'HTML',
+  //               seniority: 'Expert',
+  //             },
+  //             {
+  //               name: 'PostgreSQL',
+  //               seniority: 'Mid',
+  //             },
+  //           ],
+  //         },
+  //       },
+  //       jobs: {
+  //         create: [
+  //           {
+  //             description: 'Awesome job description',
+  //             applicantsCount: 20,
+  //             company: {
+  //               create: {
+  //                 name: 'Perfect Company',
+  //                 location: 'Romania',
+  //               },
+  //             },
+  //           },
+  //         ],
+  //       },
+  //     },
+  //   });
+};
+
+seed()
+  .then(() => {
+    client.$disconnect();
+  })
+  .catch(async (err) => {
+    console.error(err);
+    client.$disconnect();
+    process.exit(1);
+  });
