5290669d8e5bed7e2d4b01e8ba531f1dc44223d7
GeorgeCr <georgecraciun1998@gmail.com>
2024-01-06 16:01:54 +0200

    Add session

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res, Session } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  async getHello(@Res() res, @Session() session): Promise<string> {
+    console.log('session', session);
+    return res.json('OK');
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.use(
+    session({
+      name: 'job-listing',
+      cookie: {},
+      secret: 'nasa-secret',
+      resave: false,
+      saveUninitialized: true,
+      rolling: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Job Listing API')
